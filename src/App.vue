3a86eece0babdedea23f275a529c49ee7db1d8fa
Moskva_Voronezh <roman_moskvin_97@mail.ru>
2021-04-18 17:14:28 +0700

    start project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<script lang="ts">
+  import { Options, Vue } from 'vue-class-component';
+
+  @Options({
+    
+  })
+  export default class App extends Vue {
+    isInit = false;
+    async created() {
+      this.isInit = true;
+    }
+  }
+</script>
+
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+
+@Options({
+  
+})
+export default class Home extends Vue {}
+</script>
